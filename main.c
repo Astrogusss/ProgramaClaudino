802575d097ed77e77499beecdaf284898fdc6a14
gustavo <gugusenador@gmail.com>
2024-10-28 17:21:38 -0300

    Changes by Gamer

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(){
    // a massa do carro em repouso é 25% maior que o carro em movimento
     parado.massa = movimento.massa * 1.25;
 
-    // descobrir a velocidade com que o carro em repouso saia
+    // (fórmula quantidade de movimento)descobrir a velocidade com que o carro em repouso saia
     parado.velocidade = (movimento.massa * movimento.velocidade) / parado.massa;
     printf("Velocidade com que o carro vai sair para o trajeto é %0.2f \n", parado.velocidade);
 
@@ -34,7 +34,7 @@ int main(){
     // sabemos com que energia o carro em repouso ira sair
     printf("A energia potencial é: %0.2f e a energia cinetica é: %0.2f e suas somas é: %0.2f \n", potencial , cinetica , energia);
 
-    //Próxima etapa--> partir da altura 0 , e chegar ate a 5/4 da altura, qual será a nova massa que será para que ele atinga a nova altura
+    //Próxima etapa--> partir da altura 0 , e chegar ate a 5/4 da altura, qual será a nova massa necessária para que ele atinja a nova altura
     // no ponto B ele chega com o valor da variavel energia
     // somente com a energia potencial, o carro nao consegue chegar ao topo, entao a massa tem que ser reduzida, amgicamnete
 
