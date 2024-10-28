6ffae8aa13328223e5e6741f3dab0e88241a2fc2
gustavo <gugusenador@gmail.com>
2024-10-28 15:56:00 -0300

    foram adicionado funções para melhor vizualização do código principal

---
+++ programaclaudino.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "programaclaudino.h"
+#include <math.h>
+#include <stdlib.h>
+
+float cinetica(float massa, float velocidade) {
+    return (massa * (pow(velocidade,2))) / 2;
+}
+
+float potencial (float massa , float altura , float gravidade) {
+    return massa * altura * gravidade;
+}
+
+float energia(float cinetica , float potencial) {
+    return cinetica + potencial;
+}
---
+++ programaclaudino.h
@@ -0,0 +1,10 @@
+
+#ifndef PROGRAMACLAUDINO_H
+#define PROGRAMACLAUDINO_H
+float cinetica(float massa , float velocidade);
+
+float potencial(float massa , float altura , float gravidade);
+
+float energia(float cinetica , float potencial);
+
+#endif //PROGRAMACLAUDINO_H
